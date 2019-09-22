a47e85f64011a06621511992f198b644ef22378a
Pravin Patel <patelpra337@gmail.com>
2019-09-22 15:48:22 -0500

    Added colors to colortableviewcontroller

--- Rainbow_V2/ColorsTableViewController.swift
+++ Rainbow_V2/ColorsTableViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ColorsTableViewController: UITableViewController {
     
+    var colors: [Color] = [Color(name: "Red", color: UIColor.red),
+                           Color(name: "Orange", color: .orange),
+                           Color(name: "Blue", color: .blue),
+                           Color(name: "Green", color: .green),
+                           Color(name: "Yellow", color: .yellow),
+                           Color(name: "Cyan", color: .cyan),
+                           Color(name: "Purple", color: .purple)
+    ]
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        return colors.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
         
-        cell.textLabel?.text = "Cell #\(indexPath.row)"
+        let color = colors[indexPath.row]
+        
+        cell.textLabel?.text = color.name
         return cell
     }
 }
